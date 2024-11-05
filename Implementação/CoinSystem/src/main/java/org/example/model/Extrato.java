374d82d1f0ac3fc9d3c3981d92ab9e1e1b07fd72
Lucas xae <lucas.hcb0405@gmail.com>
2024-11-04 22:44:47 -0300

    arrumando bug do Id duplicado

--- Implementação/CoinSystem/src/main/java/org/example/model/Extrato.java
+++ Implementação/CoinSystem/src/main/java/org/example/model/Extrato.java
@@ -4,6 +4,8 @@ package org.example.model;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.Table;
 @Table(name = "extrato")
 public class Extrato {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String professorId;
